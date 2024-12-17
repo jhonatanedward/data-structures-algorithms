dd0618ca8efdb8bbb1590ab01783f1aa6b649aa5
Edward Jhon <jhonataneds@gmail.com>
2024-12-17 10:04:11 -0300

    Criada pasta de challenges.

---
+++ src/challenges/arrays/reverseprefixofword/ReversePrefixOfWord.java
@@ -0,0 +1,32 @@
+package challenges.arrays.reverseprefixofword;
+
+public class ReversePrefixOfWord {
+    public static void main(String[] args) {
+
+    }
+
+    public static String reversePrefix(String word, char ch) {
+        char[] teste = word.toCharArray();
+        int index = -1;
+
+        for(int i = 0; i < teste.length; i++) {
+            if(teste[i] == ch){
+                index = i;
+                break;
+            }
+        }
+
+        if(index >= 0) {
+            int left=0;
+            int right=index;
+            while(left < right) {
+                char temp = teste[left];
+                teste[left] = teste[right];
+                teste[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return new String(teste);
+    }
+}
