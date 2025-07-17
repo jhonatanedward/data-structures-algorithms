bec12f9229b4d1796532b3181c43729ee3ecf63e
Jhonatan Edward <jhonatanedsp@gmail.com>
2025-07-17 20:32:24 -0300

    The same operations was implemented using recursion

--- src/datastructures/trees/BinarySearchTree.java
+++ src/datastructures/trees/BinarySearchTree.java
@@ -15,21 +15,21 @@ public class BinarySearchTree {
 
     public boolean insert(int value) {
         Node newNode = new Node(value);
-        if(root == null) {
+        if (root == null) {
             root = newNode;
             return true;
         }
         Node temp = root;
-        while(true) {
-            if(temp.value == newNode.value) return false;
-            if(newNode.value < temp.value) {
-                if(temp.left == null) {
+        while (true) {
+            if (newNode.value == temp.value) return false;
+            if (newNode.value < temp.value) {
+                if (temp.left == null) {
                     temp.left = newNode;
                     return true;
                 }
                 temp = temp.left;
-            }else {
-                if(temp.right == null) {
+            } else {
+                if (temp.right == null) {
                     temp.right = newNode;
                     return true;
                 }
@@ -52,4 +52,73 @@ public class BinarySearchTree {
         }
         return false;
     }
+
+    private boolean rContains(Node currentNode, int value) {
+        if(currentNode == null) return false;
+
+        if(currentNode.value == value) {
+            return true;
+        }
+
+        if(value < currentNode.value) {
+            return rContains(currentNode.left, value);
+        }else {
+            return rContains(currentNode.right, value);
+        }
+    }
+
+    public boolean rContains(int value) { return rContains(root, value); }
+
+    private Node rInsert(Node currentNode, int value) {
+        if (currentNode == null) return new Node(value);
+
+        if(value < currentNode.value) {
+            currentNode.left = rInsert(currentNode.left, value);
+        }
+
+        if(value > currentNode.value) {
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+
+        return currentNode;
+    }
+
+    public void rInsert(int value) {
+        if (root == null) root = new Node(value);
+        rInsert(root, value);
+    }
+
+    public int minValue(Node currentNode) {
+        while(currentNode.left != null) {
+            currentNode = currentNode.left;
+        }
+
+        return currentNode.value;
+    }
+
+    private Node deleteNode(Node currentNode, int value) {
+        if (currentNode == null) return null;
+
+        if (value < currentNode.value) {
+            currentNode.left = deleteNode(currentNode.left, value);
+        } else if (value > currentNode.value) {
+            currentNode.right = deleteNode(currentNode.right, value);
+        } else {
+            if (currentNode.left == null && currentNode.right == null) {
+                return null;
+            } else if (currentNode.left == null) {
+                currentNode = currentNode.right;
+            } else if (currentNode.right == null) {
+                currentNode = currentNode.left;
+            } else {
+                int subTreeMin = minValue(currentNode.right);
+                currentNode.value = subTreeMin;
+                currentNode.right = deleteNode(currentNode.right, subTreeMin);
+            }
+        }
+        return currentNode;
+    }
+
+    public void deleteNode(int value) { root = deleteNode(root, value); }
+
 }
